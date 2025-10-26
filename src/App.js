bf2db858cd355df5d881e30f831884e4707548ac
jenny-int9 <candidature339@gmail.com>
2025-10-26 11:50:30 +0100

    first commit

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+/** Import de la donnée */
+import './App.css';
+
+import Robot from './data/data';
+
+function App() {
+  console.log(Robot)
+  return (
+    <div className="App">
+      <h1>Robot Finder</h1>
+    </div>
+  );
+}
+
+export default App;
